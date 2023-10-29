04be245b4b28e12a51ffdae9d3ad4c3dc638b06b
divyuk <div14kes@gmail.com>
2023-10-29 10:39:07 +0530

    Updated Comments

--- 9.0 DecoratorPattern.cpp
+++ 9.0 DecoratorPattern.cpp
@@ -1,5 +1,10 @@
 /*
 Decorator Pattern
+The Decorator Pattern is a structural design pattern that allows behavior to be added to individual objects, either statically or dynamically, without affecting the behavior of other objects from the same class
+
+💡 The key idea behind the Decorator Pattern is to create a set of decorator classes that are used to wrap concrete components. These decorators provide additional functionality while keeping the core interface of the component class intact.
+
+“Wrapper” is the alternative nickname for the Decorator pattern that clearly expresses the main idea of the pattern. A *wrapper* is an object that can be linked with some *target* object. The wrapper contains the same set of methods as the target and delegates to it all requests it receives. However, the wrapper may alter the result by doing something either before or after it passes the request to the target.
 */
 #include <iostream>
 using namespace std;
